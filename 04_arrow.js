08707e85225a34527aea234d282d7f65c63d62d4
V20100M <vincent.mochel@gmail.com>
2024-04-05 10:24:17 +0200

    add exercice 04

--- 04_arrow.js
+++ 04_arrow.js
@@ -7,7 +7,11 @@
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (a) => {
+    return a + 1;
+}
+
+console.log(arrow1(2));
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
@@ -18,6 +22,8 @@ let arrow1 = () => {}
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = a =>  a + 1;
+
+console.log(arrow2(3));
 
 module.exports = {arrow1, arrow2};
